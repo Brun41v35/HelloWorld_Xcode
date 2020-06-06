bf1524418199c4d6882757cfe493ceeda4bb422e
Bruno Alves <alves.bruno945@gmail.com>
2020-06-06 19:44:28 -0300

    Hello World iOS
    My first project in Xcode

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Bruno Silva on 05/06/20.
+//  Copyright © 2020 Bruno Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
